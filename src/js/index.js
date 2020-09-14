e1f949458fc27ba78496238f90fdeb1678e20d4c
Paulina Studniak <paulinaw2612@gmail.com>
2020-09-14 13:21:59 +0200

    added script with list of repositories to github account-fetch api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from "moment";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,40 +6,54 @@ import moment from 'moment';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
-const firstName = 'Pauli';
+console.log("HELLO 🚀");
+const firstName = "Pauli";
 const age = 32;
 
+console.log(
+  `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lata.`
+);
 
-console.log(`Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lata.`)
-
-const emptyParagraph = document.querySelector('.week-summary__description--js');
+const emptyParagraph = document.querySelector(".week-summary__description--js");
 
 emptyParagraph.innerHTML = `Uzupełniam treść javascriptem`;
 
-
-function greet (firstName, age){
-    console.log(`Witaj, nazywam się ${firstName} i mam ${age} lata.`)
+function greet(firstName, age) {
+  console.log(`Witaj, nazywam się ${firstName} i mam ${age} lata.`);
 }
 
 greet(firstName, age);
-greet('Ania', 22);
+greet("Ania", 22);
 
-const greetNew = (firstName, age) =>{
-    console.log(`Witaj, nazywam się ${firstName} i mam ${age} lata.`)
-}
+const greetNew = (firstName, age) => {
+  console.log(`Witaj, nazywam się ${firstName} i mam ${age} lata.`);
+};
 greetNew(firstName, age);
 
-const button = document.querySelector('.action--js');
+const button = document.querySelector(".action--js");
 
-const myClick = () =>{
-    const heading = document.querySelector('.main__heading--js');
-    heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lata.`
-}
+const myClick = () => {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lata.`;
+};
 
-button.addEventListener('click',myClick);
+button.addEventListener("click", myClick);
 
-const startOfDay = moment().startOf('day').fromNow();
+const startOfDay = moment().startOf("day").fromNow();
 
-const timePlaceholder = document.querySelector('.time--js');
+const timePlaceholder = document.querySelector(".time--js");
 timePlaceholder.innerHTML = startOfDay;
+
+fetch("https://api.github.com/users/paulina-studniak/repos")
+  .then(resp => resp.json())
+  .then(resp => {
+    for (let repo of resp) {
+      const { name, html_url } = repo;
+      const repositoryList = document.querySelector('.list--js');
+      const myTemplate = `<li> ${name} <a href="${html_url}" title="link do repozytorium ${name}">link do githuba </a> </li>`;
+;
+    repositoryList.innerHTML += myTemplate;}
+  })
+  .catch((error) => {
+    console.log("nie udało się pobrać");
+  });
